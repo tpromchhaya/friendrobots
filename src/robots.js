261d4e2121a0ae590aca2486b20892d7aae930b2
tpromchhaya <promchhaya.than@gmail.com>
2020-07-18 19:12:56 -0400

    initial push

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+{
+	id:1,
+	name: 'Snake',
+	username: 'JakeTheSnake',
+	email: 'Snake@snake.ca'
+
+},
+{
+	id:2,
+	name: 'Beaver',
+	username: 'JustinBeaver',
+	email: 'Justin@Beavertown.com'
+
+},
+{
+	id:3,
+	name: 'Moose',
+	username: 'MooseUSA',
+	email: 'MooseUSA@USA.us'
+
+},
+{
+	id:4,
+	name: 'Llama',
+	username: 'Llama1',
+	email: 'Llama1@Llama1.com'
+
+},
+{
+	id:5,
+	name: 'Lion',
+	username: 'LionKing',
+	email: 'LionKing@yoyo.com'
+
+},
+{
+	id:6,
+	name: 'Tiger',
+	username: 'SouthernTiger',
+	email: 'SouthernTiger@south.mexica'
+
+},
+{
+	id:7,
+	name: 'Dolphin',
+	username: 'DolphinSea',
+	email: 'DolphinSea@bigsea.com'
+
+},
+{
+	id:8,
+	name: 'Monkey',
+	username: 'MountainMonkey',
+	email: 'MountainMonkey@asia.asia'
+
+},
+{
+	id:9,
+	name: 'Horse',
+	username: 'RunningHorse',
+	email: 'R.Horse@asia.asia'
+
+},
+{
+	id:10,
+	name: 'Oyster',
+	username: 'RiverOyster',
+	email: 'RiverOyster@vn.3000'
+
+},
+
+
+]
